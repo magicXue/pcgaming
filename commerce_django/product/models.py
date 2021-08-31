1fa27cbd59c112033464611e0a80bb710c22e5ac
David <davidchenxue@gmail.com>
2021-09-01 07:47:43 +1000

    create class product detail and get_object and get function in django.

--- commerce_django/product/models.py
+++ commerce_django/product/models.py
@@ -30,7 +30,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     def get_absolute_url(self):
-        return f'{self.category.slug}/{self.slug}/'
+        return f'/{self.category.slug}/{self.slug}/'
     
     def get_image(self):
         if self.image:
